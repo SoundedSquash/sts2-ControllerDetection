6ec6a009038735aab503d5d9182d631c277c4354
SoundedSquash <20405599+SoundedSquash@users.noreply.github.com>
2026-04-20 23:26:16 -0500

    Initial prototype.

---
+++ ToggleController/ModEntry.cs
@@ -0,0 +1,12 @@
+﻿using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
+
+[ModInitializer("Initialize")]
+public class ModEntry
+{
+    public static void Initialize()
+    {
+        var harmony = new Harmony("soundedsquash.togglecontroller");
+        harmony.PatchAll();
+    }
+}
---
+++ ToggleController/Patches/NControllerManagerPatch.cs
@@ -0,0 +1,29 @@
+﻿using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.ControllerInput;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
+
+namespace SaveManager.Patches;
+
+[HarmonyPatch(typeof(NControllerManager))]
+public class NControllerManagerPatch
+{
+    [HarmonyPatch(nameof(NControllerManager._Input))]
+    [HarmonyPrefix]
+    static bool _InputPatch(InputEvent inputEvent, NControllerManager __instance)
+    {
+        if (__instance.IsUsingController) return true;
+        
+        // Simulate a controller button and have it do its normal controller checks/updates.
+        inputEvent = new InputEventAction()
+        {
+            Action = Controller.AllControllerInputs.FirstOrDefault(),
+            Pressed = true
+        };
+
+        var method = AccessTools.Method(typeof(NControllerManager), "CheckForControllerInput");
+        method.Invoke(__instance, [inputEvent]);
+        return true;
+    }
+}
